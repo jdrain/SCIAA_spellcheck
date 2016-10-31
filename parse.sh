994c0f85bb019d2b1ab7e9bfe004d67a40630a33
jdrain <jdrain@email.sc.edu>
2016-10-31 12:45:11 -0400

    deleted extraneous files

--- parse.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-#spellchecks processed text files and then parses them
-#takes one command line arg which is a directory containing text files
-
-for i in ls $1; do python ../SCIAA_spellcheck/checkAndParse.py $i >> checkAndParseTest.txt; done;
