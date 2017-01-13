2b540fb698790e54a4c22f3f8bf3de6ccf7e7c70
jdrain <jdrain@email.sc.edu>
2017-01-13 14:46:18 -0500

    rewrote the script so that it takes in an entire directory path in lieu of just a filepath. This allows the software to iteratively parse the txt in one go instead of running the script multiple times.  Much more efficient than the previous approach.

--- main.py
+++ main.py
@@ -7,51 +7,62 @@ from textProcessing import processData
 TODO:
     -add function to get the type of form
     -change code to add to the csv instead of creating a new one
+    -needs functionality to take in a directory and iterate over it instead
+     of having to run this script once for each file. Super inefficient.
 """
 
-filePath=sys.argv[1]
-csvPath=sys.argv[2]
-fileList=parser.read_file_simple(filePath)
+#input dir and output path
+dir_path=sys.argv[1]
+dbf_csv_path=sys.argv[2]
 
-print("\nsource file: "+filePath+"\n")
-
-#get data from json
+#get data from json files
 keys=processData.processJSON("./textProcessing/keys1.json")
 encoding_keys=processData.processJSON("./textProcessing/Encodings.json")
-#print keys for debug
+db_field_coordinates=processData.processJSON("./textProcessing/DatabaseFieldCoordinates.json")
+
 """
-for key in keys:
-    print(key)
+filePath=sys.argv[1] #path to the file
+csvPath=sys.argv[2] #csv output path
+dbf_csv_path="../JustAllendale.csv" #path for the dbf csv
 """
+for fpath in os.listdir(dir_path):
 
-#parse
-print("\nparser is removing underlines: \n")
-fileList=parser.remove_underlines(fileList)
+    fileList=parser.read_file_simple(dir_path+"/"+fpath)
+    csvOut=processData.readCSV(dbf_csv_path)
 
-print("\nparser is removing newline nums: \n")
-fileList=parser.remove_newline_nums(fileList)
+    print("\nsource file: "+fpath+"\n")
 
-print("\nparser is choosing best matches: \n")
-parsed=parser.filter_potential_data(keys["1985"],fileList)
+    #parse
+    print("\nparser is removing underlines: \n")
+    fileList=parser.remove_underlines(fileList)
 
-#print results
-print("\nparsed: \n")
-for i in parsed:
-    print(i)
+    print("\nparser is removing newline nums: \n")
+    fileList=parser.remove_newline_nums(fileList)
 
-extracted=parser.extract_data(fileList,parsed,keys["1985"])
+    print("\nparser is choosing best matches: \n")
+    parsed=parser.filter_potential_data(keys["1985"],fileList)
 
-print("\nextracted: \n")
-for i in extracted:
-    print(i)
+    #print results
+    print("\nparsed: \n")
+    for i in parsed:
+        print(i)
 
-formatted=parser.database_format(extracted,keys["1985"],encoding_keys)
+    extracted=parser.extract_data(fileList,parsed,keys["1985"])
 
-print("\nformatted: \n")
-for i in formatted:
-    print(i)
+    print("\nextracted: \n")
+    for i in extracted:
+        print(i)
 
-#compress and write to file
-print("\ncompressing and writing to file\n")
-compressed=processData.compress_list(formatted)
-processData.toCSV(csvPath,compressed)
+    formatted=parser.database_format(extracted,keys["1985"],encoding_keys)
+
+    print("\nformatted: \n")
+    for i in formatted:
+        print(i)
+
+    #compress and write to file
+    print("\ncompressing:\n")
+    compressed=processData.compress_list(formatted)
+
+    #writing to the dbf csv
+    print("\nwriting to dbf file:")
+    processData.write_to_dbf(fpath,compressed,db_field_coordinates,csvOut,dbf_csv_path)
